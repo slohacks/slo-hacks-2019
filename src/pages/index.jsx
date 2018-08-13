1d12321553edf879304bb701c24260785ae67886
Selynna Sun <ssun098@gmail.com>
2018-08-12 23:49:26 -0700

    feat(layouts, pages, utils): Add base React files

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const IndexPage = () => (
+  <div>
+  </div>
+);
+export default IndexPage;
---
+++ src/utils/typography.js
@@ -0,0 +1,18 @@
+import Typography from 'typography';
+
+const typography = new Typography({
+  baseFontSize: '16px',
+  headerFontFamily: [
+    'Helvetica Neue',
+    'Helvetica',
+    'sans-serif',
+  ],
+  headerColor: '#465A64',
+  bodyFontFamily: [
+    'Helvetica Neue',
+    'Helvetica',
+    'sans-serif',
+  ],
+});
+
+export default typography;
