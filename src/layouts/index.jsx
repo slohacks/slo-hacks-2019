8d3ae44a7b247620b4e7d7d9e88006c771050cae
Selynna Sun <ssun098@gmail.com>
2018-08-15 16:10:50 -0700

    fix(lint): fix stylelint

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,25 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import './index.css';
-import styles from './index.module.css';
 
-class TemplateWrapper extends React.Component {
-  render() {
-    return (
-      <div>
-        <Helmet
-          title="SLO Hacks"
-          meta={[
-            { name: 'description', content: 'SLO Hacks' },
-            { name: 'keywords', content: 'san luis obispo, cal poly, hackathon, coding, software, competition' },
-            { name: 'theme-color', content: '#FFFFFF' },
-          ]}
-        />
-        <p>Hello world</p>
-      </div>
-    );
-  }
-}
+const TemplateWrapper = props => (
+  <div>
+    <Helmet
+      title="SLO Hacks"
+      meta={[
+        { name: 'description', content: 'SLO Hacks' },
+        { name: 'keywords', content: 'san luis obispo, cal poly, hackathon, coding, software, competition' },
+        { name: 'theme-color', content: '#FFFFFF' },
+      ]}
+    />
+    <div>
+      {props.children()}
+    </div>
+  </div>
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func, // eslint-disable-line react/require-default-props
