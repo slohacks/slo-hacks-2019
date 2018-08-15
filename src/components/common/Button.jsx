896474dd32d7fce8656f996983db6dc927844cc8
Selynna Sun <ssun098@gmail.com>
2018-08-15 15:23:47 -0700

    fix(Button): fix typo with border

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
 const Button = ({ children, buttonText, color, fontSize }) => {
-  const border = "3px solid" + color;
+  const border = "3px solid " + color;
   const buttonStyling = { color, border, fontSize };
   return (
     <div style={buttonStyling} className={styles.button}>
