021b01414fb9921f52f7e33213829f7b5735ba6e
Selynna Sun <ssun098@gmail.com>
2018-10-27 19:21:34 -0700

    fix(Footer): add footer images (#26)

--- src/components/common/SocialMediaIcon.jsx
+++ src/components/common/SocialMediaIcon.jsx
@@ -3,52 +3,20 @@ import PropTypes from 'prop-types';
 import styles from './Footer.module.css';
 
 class SocialMediaIcon extends React.Component {
-  state = { image: "./static/footer/social_" + this.props.icon + "-01.svg" };
-  props = {
-    icon: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired
-  }
-
-  handleMouseOver = () => {
-    const imageUrl = "./static/footer/social_" + this.props.icon + "-02.svg";
-    this.setState({ image: imageUrl });
-  }
-
-  handleMouseOut = () => {
-    const imageUrl = "./static/footer/social_" + this.props.icon + "-01.svg";
-    this.setState({ image: imageUrl });
-  }
-
-  handleOnClick = () => {
-    const imageUrl = "./static/footer/social_" + this.props.icon + "-01.svg";
-    this.setState({ image: imageUrl });
-  }
-
-  handleTouchStart = () => {
-    const imageUrl = "./static/footer/social_" + this.props.icon + "-02.svg";
-    this.setState({ image: imageUrl });
-  }
-
-  handleTouchEnd = () => {
-    const imageUrl = "./static/footer/social_" + this.props.icon + "-01.svg";
-    this.setState({ image: imageUrl });
+  constructor(props) {
+    super(props);
   }
 
   render() {
     let styling = styles.footer__icon;
-    if (this.props.icon === "twitter") {
+    if (this.props.url === "https://twitter.com/slohacks") {
       styling = styles.footer__icon + ' ' + styles.footer__twitter;
     }
 
     return (
       <div className={styles.footer_row}>
         <a rel="noopener" href={this.props.url} target="_blank">
-          <img className={styling} src={this.state.image} 
-            onMouseOver={this.handleMouseOver}
-            onMouseOut={this.handleMouseOut}
-            onClick={this.handleOnClick}
-            onTouchStart={this.handleTouchStart}
-            onTouchEnd={this.handleTouchEnd} />
+          <img className={styling} src={this.props.icon} />
         </a>
       </div>
     );
