7a8eddf18a933f812d14dc1c4a7091bc9274f550
Mian Uddin <mianuddin@gmail.com>
2018-11-04 19:33:44 -0800

    feat(package.json, typography.js): set main font family to Brandon Grotesque

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,12 +3,14 @@ import Typography from 'typography';
 const typography = new Typography({
   baseFontSize: '16px',
   headerFontFamily: [
+    'Brandon Grotesque',
     'Helvetica Neue',
     'Helvetica',
     'sans-serif',
   ],
   headerColor: '#465A64',
   bodyFontFamily: [
+    'Brandon Grotesque',
     'Helvetica Neue',
     'Helvetica',
     'sans-serif',
