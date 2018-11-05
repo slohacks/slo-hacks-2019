8b9f21cb2cd445ea4860914794e97d7ad9c37ed1
Mian Uddin <mianuddin@gmail.com>
2018-11-04 20:21:37 -0800

    feat(components/): standardize paragraph sizes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => (
     <section className={styles.aboutContainer}>
       <div className={styles.aboutContent}>
         <h1 className={styles.aboutHeader}>Turning dreams into reality.</h1>
-        <TextSection fontSize="1.3rem" color="#FFF">
+        <TextSection>
           SLO Hacks 2019 is a hackathon event hosted at Cal Poly, San Luis
           Obispo. Over a period of 36 hours, you will meet new people, learn new
           skills, and turn your ideas into a reality!
