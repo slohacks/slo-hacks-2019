bd64c3c9ff186eec1b37519af5e2bd67f02a3e23
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-11-06 02:22:36 -0800

    feat(src/hero.jsx): add MLH badge to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,6 +34,9 @@ const Hero = () => (
         </div>
       </div>
     </section>
+    <a className={styles.banner} rel="noopener noreferrer" href="https://mlh.io/seasons/na-2019/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2019-season&utm_content=black" target="_blank">
+      <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-black.svg" alt="Major League Hacking 2019 Hackathon Season" />
+    </a>
   </div>
 );
 
