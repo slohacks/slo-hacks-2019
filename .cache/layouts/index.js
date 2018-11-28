4e2f3f94aa53572c6bca0a682dc3446115922f1c
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-11-28 02:41:43 -0800

    Revert "Add latest build"
    This reverts commit cbdac6ae8cf1ba34865f068bc5ab9896dd664c87.

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/mnt/c/Users/jumer/event-dev/src/layouts/index.jsx"
-  import data from "/mnt/c/Users/jumer/event-dev/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
