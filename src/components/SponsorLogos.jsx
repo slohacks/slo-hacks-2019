7df0359c3c99403ab47e4fa1ab2e0c8994d0cf8c
Selynna Sun <ssun098@gmail.com>
2018-11-28 08:50:32 +0200

    Revert "feat(SponsorLogos.jsx): add new sponsors (#42)" (#43)
    This reverts commit 3314a48d1f4eb051ccac7142c82acefd9bbd4fae.

--- src/components/SponsorLogos.jsx
+++ src/components/SponsorLogos.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import godaddy from '../assets/logos/godaddy.svg';
 import balsamiq from '../assets/logos/balsamiq.svg';
 import gcp from '../assets/logos/gcp.svg';
-import micro from '../assets/logos/microvu.svg';
-import viasat from '../assets/logos/viasat.png';
-import sketch from '../assets/logos/sketch.svg';
-import td from '../assets/logos/tech.png';
-import wolfram from '../assets/logos/wolfram.svg';
 import styles from './SponsorLogos.module.css';
 
 const SponsorLogos = () => (
@@ -14,11 +9,6 @@ const SponsorLogos = () => (
     <img className={styles.silver} src={godaddy} alt="godaddy" />
     <img className={styles.silver} src={gcp} alt="gcp" />
     <img className={styles.silver} src={balsamiq} alt="balsamiq" />
-    <img className={styles.silver} src={micro} alt="micro" />
-    <img className={styles.silver} src={viasat} alt="viasat" />
-    <img className={styles.silver} src={sketch} alt="sketch" />
-    <img className={styles.silver} src={td} alt="techdomain" />
-    <img className={styles.silver} src={wolfram} alt="wolfram" />
   </div>
 );
 
