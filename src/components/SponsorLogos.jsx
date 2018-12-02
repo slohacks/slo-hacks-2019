789ea71ec815aa698df3de7b64cf37e1b8bc464e
Jumer Ryan Caragay <jumer.caragay@gmail.com>
2018-12-01 16:46:00 -0800

    feat(SponsorLogos.jsx, SponsorLogos.module.css): add github and cpe logo

--- src/components/SponsorLogos.jsx
+++ src/components/SponsorLogos.jsx
@@ -6,11 +6,13 @@ import micro from '../assets/logos/microvu.svg';
 import viasat from '../assets/logos/viasat.png';
 import splunk from '../assets/logos/splunk.png';
 import sketch from '../assets/logos/sketch.svg';
+import github from '../assets/logos/github.png';
 import td from '../assets/logos/tech.png';
 import wolfram from '../assets/logos/wolfram.svg';
 import mule from '../assets/logos/stickermule.svg';
 import hathway from '../assets/logos/hathway.svg';
 import calpolycs from '../assets/logos/calpolycs.png';
+import cpe from '../assets/logos/cpe.png';
 import styles from './SponsorLogos.module.css';
 
 const SponsorLogos = () => (
@@ -21,15 +23,17 @@ const SponsorLogos = () => (
       <img className={styles.bigLogo} src={godaddy} alt="godaddy" />
     </div>
     <div className={styles.smallLogos}>
-      <img className={styles.silver} src={gcp} alt="gcp" />
-      <img className={styles.silver} src={hathway} alt="hathway" />
-      <img className={styles.silver} src={splunk} alt="splunk" />
-      <img className={styles.silver} src={balsamiq} alt="balsamiq" />
-      <img className={styles.silver} src={wolfram} alt="wolfram" />
-      <img className={styles.silver} src={sketch} alt="sketch" />
-      <img className={styles.silver} src={td} alt="techdomain" />
-      <img className={styles.silver} src={mule} alt="stickermule" />
-      <img className={styles.silver} src={calpolycs} alt="calpoly" />
+      <img className={styles.silver} src={gcp} alt="GCP" />
+      <img className={styles.silver} src={hathway} alt="Hathway" />
+      <img className={styles.silver} src={splunk} alt="Splunk" />
+      <img className={styles.silver} src={github} alt="Github" />
+      <img className={styles.silver} src={balsamiq} alt="Balsamiq" />
+      <img className={styles.silver} src={wolfram} alt="Wolfram" />
+      <img className={styles.silver} src={sketch} alt="Sketch" />
+      <img className={styles.silver} src={td} alt="TechDomain" />
+      <img className={styles.silver} src={mule} alt="StickerMule" />
+      <img className={styles.silver} src={calpolycs} alt="Cal Poly CS" />
+      <img className={styles.silver} src={cpe} alt="Cal Poly CPE" />
     </div>
   </div>
 );
