8565fc193bc290baf0bf502016db8876ed48e74f
stonewallstan <stanleyarmstrong31@gmail.com>
2019-01-15 22:39:26 -0800

    feat(SponsorLogos): add consistent styling throughout the sponsor page

--- src/components/SponsorLogos.jsx
+++ src/components/SponsorLogos.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import f5 from '../assets/logos/f5-logo.png';
 import wdtemp from '../assets/logos/wd-temp.svg';
-import itrade from '../assets/logos/itrade.svg';
+import itrade from '../assets/logos/itrade.png';
 import viasat from '../assets/logos/viasat.png';
 import godaddy from '../assets/logos/godaddy.svg';
 import micro from '../assets/logos/microvu.svg';
@@ -25,81 +25,81 @@ import styles from './SponsorLogos.module.css';
 const SponsorLogos = () => (
   <div className={styles.sponsorLogos}>
     <div className={styles.topSponsors}>
-      <a className={`${styles.topLogo} ${styles.f5}`} href="https://www.f5.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.topLogo} src={f5} alt="F5 Networks" />
+      <a className={styles.topLogo} href="https://www.f5.com/" target="_blank" rel="noopener noreferrer">
+        <img src={f5} alt="F5 Networks" />
       </a>
-      <a className={`${styles.topLogo} ${styles.wdtemp}`} href="https://www.westerndigital.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.topLogo} src={wdtemp} alt="Western Digital" />
+      <a className={styles.topLogo} href="https://www.westerndigital.com/" target="_blank" rel="noopener noreferrer">
+        <img src={wdtemp} alt="Western Digital" />
       </a>
-      <a className={`${styles.topLogo} ${styles.itrade}`} href="https://www.itradenetwork.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.topLogo} src={itrade} alt="iTradeNetwork" />
+      <a className={styles.topLogo} href="https://www.itradenetwork.com/" target="_blank" rel="noopener noreferrer">
+        <img src={itrade} alt="iTradeNetwork" />
       </a>
     </div>
     <div className={styles.medSponsors}>
-      <a className={`${styles.medLogo} ${styles.viasat}`} href="https://www.viasat.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.medLogo} src={viasat} alt="Viasat" />
+      <a className={styles.medLogo} href="https://www.viasat.com/" target="_blank" rel="noopener noreferrer">
+        <img src={viasat} alt="Viasat" />
       </a>
-      <a className={`${styles.medLogo} ${styles.godaddy}`} href="https://www.godaddy.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.medLogo} src={godaddy} alt="GoDaddy" />
+      <a className={styles.medLogo} href="https://www.godaddy.com/" target="_blank" rel="noopener noreferrer">
+        <img src={godaddy} alt="GoDaddy" />
       </a>
-      <a className={`${styles.medLogo} ${styles.micro}`} href="https://www.microvu.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.medLogo} src={micro} alt="Micro Vu" />
+      <a className={styles.medLogo} href="https://www.microvu.com/" target="_blank" rel="noopener noreferrer">
+        <img src={micro} alt="Micro Vu" />
       </a>
-      <a className={`${styles.medLogo} ${styles.platinum}`} href="https://www.platinumperformance.com/horses" target="_blank" rel="noopener noreferrer">
-        <img className={styles.medLogo} src={platinum} alt="Platinum Performance" />
-      </a>
-    </div>
-    <div className={styles.smallLogos}>
-      <a className={`${styles.silver} ${styles.gcp}`} href="https://cloud.google.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={gcp} alt="Google Cloud Platform" />
-      </a>
-      <a className={`${styles.silver} ${styles.splunk}`} href="https://www.splunk.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={splunk} alt="Splunk" />
-      </a>
-      <a className={`${styles.silver} ${styles.github}`} href="https://github.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={github} alt="GitHub" />
-      </a>
-      <a className={`${styles.silver} ${styles.hathway}`} href="https://wearehathway.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={hathway} alt="Hathway" />
+      <a className={styles.medLogo} href="https://www.platinumperformance.com/horses" target="_blank" rel="noopener noreferrer">
+        <img src={platinum} alt="Platinum Performance" />
       </a>
     </div>
     <div className={styles.smallLogos}>
-      <a className={`${styles.silver} ${styles.balsamiq}`} href="https://balsamiq.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={balsamiq} alt="Balsamiq" />
+      <a className={styles.silver} href="https://cloud.google.com/" target="_blank" rel="noopener noreferrer">
+        <img src={gcp} alt="Google Cloud Platform" />
       </a>
-      <a className={`${styles.silver} ${styles.linode}`} href="https://www.linode.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={linode} alt="Linode" />
+      <a className={styles.silver} href="https://www.splunk.com/" target="_blank" rel="noopener noreferrer">
+        <img src={splunk} alt="Splunk" />
       </a>
-      <a className={`${styles.silver} ${styles.flume}`} href="https://www.flumetech.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={flume} alt="Flume" />
+      <a className={styles.silver} href="https://github.com/" target="_blank" rel="noopener noreferrer">
+        <img src={github} alt="GitHub" />
       </a>
-      <a className={`${styles.silver} ${styles.tech}`} href="https://get.tech/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={td} alt=".Tech" />
+      <a className={styles.silver} href="https://wearehathway.com/" target="_blank" rel="noopener noreferrer">
+        <img src={hathway} alt="Hathway" />
       </a>
     </div>
     <div className={styles.smallLogos}>
-      <a className={`${styles.silver} ${styles.corsair}`} href="https://www.corsair.com/us/en/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={corsair} alt="Corsair" />
+      <a className={styles.silver} href="https://balsamiq.com/" target="_blank" rel="noopener noreferrer">
+        <img src={balsamiq} alt="Balsamiq" />
       </a>
-      <a className={`${styles.silver} ${styles.wolfram}`} href="https://www.wolfram.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={wolfram} alt="Wolfram Language" />
+      <a className={styles.silver} href="https://www.linode.com/" target="_blank" rel="noopener noreferrer">
+        <img src={linode} alt="Linode" />
       </a>
-      <a className={`${styles.silver} ${styles.sketch}`} href="https://www.sketchapp.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={sketch} alt="Sketch" />
+      <a className={styles.silver} href="https://www.flumetech.com/" target="_blank" rel="noopener noreferrer">
+        <img src={flume} alt="Flume" />
       </a>
-      <a className={`${styles.silver} ${styles.stickermule}`} href="https://www.stickermule.com/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={mule} alt="Stickermule" />
+      <a className={styles.silver} href="https://get.tech/" target="_blank" rel="noopener noreferrer">
+        <img src={td} alt=".Tech" />
+      </a>
+    </div>
+    <div className={styles.smallLogos}>
+      <a className={styles.silver} href="https://www.corsair.com/us/en/" target="_blank" rel="noopener noreferrer">
+        <img src={corsair} alt="Corsair" />
+      </a>
+      <a className={styles.silver} href="https://www.wolfram.com/" target="_blank" rel="noopener noreferrer">
+        <img src={wolfram} alt="Wolfram Language" />
+      </a>
+      <a className={styles.silver} href="https://www.sketchapp.com/" target="_blank" rel="noopener noreferrer">
+        <img src={sketch} alt="Sketch" />
+      </a>
+      <a className={styles.silver} href="https://www.stickermule.com/" target="_blank" rel="noopener noreferrer">
+        <img src={mule} alt="Stickermule" />
       </a>
     </div>
     <div className={`${styles.smallLogos}`}>
-      <br className={styles.silver} />
-      <a className={`${styles.silver} ${styles.csc}`} href="https://csc.calpoly.edu/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={calpolycs} alt="Cal Poly Computer Science" />
+      <div className={styles.silver} />
+      <a className={styles.silver} href="https://csc.calpoly.edu/" target="_blank" rel="noopener noreferrer">
+        <img src={calpolycs} alt="Cal Poly Computer Science" />
       </a>
-      <a className={`${styles.silver} ${styles.cpe}`} href="https://cpe.calpoly.edu/" target="_blank" rel="noopener noreferrer">
-        <img className={styles.silver} src={cpe} alt="Cal Poly Computer Engineering" />
+      <a className={styles.silver} href="https://cpe.calpoly.edu/" target="_blank" rel="noopener noreferrer">
+        <img src={cpe} alt="Cal Poly Computer Engineering" />
       </a>
-      <br className={styles.silver} />
+      <div className={styles.silver} />
     </div>
   </div>
 );
